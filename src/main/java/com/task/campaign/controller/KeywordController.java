7f4de45ea8a4aa94ae1394d4b9eef86e6704bd70
Sebastian Gałan <sebastian.galan7625@gmail.com>
2024-07-26 13:35:37 +0200

    Created a Keyword model with controller and service

---
+++ src/main/java/com/task/campaign/controller/KeywordController.java
@@ -0,0 +1,27 @@
+package com.task.campaign.controller;
+
+import com.task.campaign.database.model.City;
+import com.task.campaign.database.model.Keyword;
+import com.task.campaign.service.CityService;
+import com.task.campaign.service.KeywordService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Data
+@RestController
+@RequestMapping("/api/keyword")
+@RequiredArgsConstructor
+public class KeywordController {
+    final KeywordService keywordService;
+
+    @GetMapping("/search")
+    public List<Keyword> findByPrefix(@RequestParam String prefix){
+        return keywordService.findByPrefix(prefix);
+    }
+}
